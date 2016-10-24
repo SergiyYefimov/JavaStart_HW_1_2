7403f99e2c027aa288eb3559705e8fd3f135291c
Єфімов Сергій Леонідович <s.efimov@md.mdclinics.com.ua>
2016-10-24 14:05:08 +0300

    Task complite

---
+++ src/homeTask2/taskTwo.java
@@ -0,0 +1,13 @@
+package homeTask2;
+
+public class taskTwo {
+
+	public static void main(String[] args) {
+		double a=5, b=6, c=7, s, p;
+		p=(a+b+c)/2;
+		s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		System.out.println("Rectangle square is: "+s);
+
+	}
+
+}
